d602ee4a379915a23e162ca5966bdb5ef221943c
tian <963766613@qq.com>
2019-04-01 20:34:03 +0800

    0401

---
+++ two_func/two_func/two_func.c
@@ -0,0 +1,15 @@
+//一个文件中包含连个函数
+#include <stdio.h>
+void butler(void);/* ANSI/ISO C函数原型 */
+int main(void)
+{
+	printf("I will summon the butler function.\n"); //summon调用
+	butler();
+	printf("yes. Bring me some tea and writeable DVDs.\n");
+
+	return 0;
+}
+void butler(void) /* 函数定义开始 */
+{
+	printf("You rang, sir?\n");
+}

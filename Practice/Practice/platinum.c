5d938421f5f37217b8eaebcdd02f87f1fc2f6d22
tian <963766613@qq.com>
2019-04-02 10:49:52 +0800

    0402

---
+++ Practice/Practice/platinum.c
@@ -0,0 +1,21 @@
+//you weight in platinum
+#include <stdio.h>
+int main(void)
+{
+	float weight;//体重
+	float value; //价值
+
+	printf("Are you worth your weight in platinum?\n");
+	printf("Let's check it out.\n");
+	printf("Please enter your weight in pounds: ");
+
+	//获取用户输入
+	scanf("%f", &weight);
+	//假设白金价格是每盎司$1700
+	value = 1700.0 * weight*14.5833;
+	printf("Your weight in platinum is worth $%.2f.\n", value);
+	printf("You are easily worth that! If platinum prices drop,\n");
+	printf("eat more to maintain your value.\n");
+
+	return 0;
+}

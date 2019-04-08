b2f7aab9b885317bb5928531d2b422da62f5f1aa
tian <963766613@qq.com>
2019-04-08 16:50:41 +0800

    0408

---
+++ Practice/Practice/add_one.c
@@ -0,0 +1,14 @@
+//ǰ׺�ͺ�׺
+#include <stdio.h>
+int main(void)
+{
+	int ultra = 0, super = 0;
+
+	while (super < 5)
+	{
+		super++;
+		++ultra;
+		printf("super = %d,ultra = %d \n", super, ultra);
+	}
+	return 0;
+}

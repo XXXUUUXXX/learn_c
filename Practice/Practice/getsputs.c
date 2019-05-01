3ec52cca316a5005cab5873a7320c2f977ca1d5a
tian <963766613@qq.com>
2019-05-01 16:54:09 +0800

    0501

---
+++ Practice/Practice/getsputs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define STLEN 81
+int main(void)
+{
+	char words[STLEN];
+
+	puts("enter a string,please.");
+	gets(words);
+	printf("your string twice:\n");
+	printf("%s\n", words);
+	puts(words);
+	puts("done!");
+
+	return 0;
+}

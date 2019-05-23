de101b79f261c2a288de6cfbea4e3486b529fa0d
tian <963766613@qq.com>
2019-05-23 22:13:35 +0800

    0523

---
+++ Practice/Practice/hiding.c
@@ -0,0 +1,22 @@
+//块中的变量
+#include <stdio.h>
+int main()
+{
+	int x = 30;//原始x
+
+	printf("x in outer block: %d at %p\n", x, &x);
+	{
+		int x = 77;//新x
+		printf("x in inner block: %d at %p\n", x, &x);
+	}
+	printf("x in outer block: %d at %p\n", x, &x);
+	while (x++ < 33)
+	{
+		int x = 100;
+		x++;
+		printf("x in while loop: %d at %p\n", x, &x);
+	}
+	printf("x in outer block: %d at %p", x, &x);
+
+	return 0;
+}
